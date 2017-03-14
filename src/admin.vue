efeefe92861c4a48a1ae8fdcfb1a18b99e566e6b
Josh Gillespie <josh@bethel.io>
2017-03-13 23:34:31 -0500

    Add day creation form

--- src/admin.vue
+++ src/admin.vue
@@ -9,7 +9,7 @@
       return {
         msg: "Admin View",
         newDay: {
-          chapter: ''
+          chapter: 1
         },
         chapterOptions: chapters
       };
@@ -20,11 +20,40 @@
 
 <template>
   <div id="admin">
-    <select v-model="newDay.chapter">
-      <option v-for="(option, index) in chapterOptions" v-bind:value="option">
-        {{ index + 1 }}: {{ option }}
-      </option>
-    </select>
-    <span>Chapter: {{ newDay.chapter }}</span>
+
+    <div>
+      <label for="chapter">Chapter: </label>
+      <select id="chapter" v-model="newDay.chapter">
+        <option v-for="(option, index) in chapterOptions" v-bind:value="index + 1">
+          {{ index + 1 }}: {{ option }}
+        </option>
+      </select>
+    </div>
+
+    <div>
+      <label for="lat">Latitude: </label>
+      <input id="lat" v-model.number="newDay.lat" type="number">
+    </div>
+
+    <div>
+      <label for="long">Longitude: </label>
+      <input id="long" v-model.number="newDay.long" type="number">
+    </div>
+
+    <div>
+      <label for="leagues">Leagues: </label>
+      <input id="leagues" v-model.number="newDay.leagues" type="number">
+    </div>
+
+    <div>
+      <label for="excerpt">Excerpt: </label>
+      <textarea id="excerpt" v-model="newDay.excerpt"></textarea>
+    </div>
+
+    <div>
+      <label for="location">Location: </label>
+      <input id="location" v-model="newDay.location" type="text">
+    </div>
+
   </div>
 </template>
