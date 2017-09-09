2a962964771bfc417e5688c9728cb355dfd9a61e
Josh Gillespie <josh@bethel.io>
2017-09-08 21:47:47 -0500

    add formatted date component

---
+++ src/components/formatted-date.vue
@@ -0,0 +1,13 @@
+<script type="text/javascript">
+  export default {
+    name: 'formatted-date',
+    props: ['day']
+  };
+</script>
+
+<template>
+  <span>
+      <span v-if="day">{{ day.month }}/{{ day.day }}/{{ day.year }}</span>
+      <span v-else>Select Day</span>
+  </span>
+</template>
